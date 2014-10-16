080f937d4dcaba6f26722d32eda3eafdf1d8d6bf
209030 <209030@TPC0596.tec-inf.local>
2014-10-16 18:35:44 +0900

    add: oauth

--- app/models/cms/member.rb
+++ app/models/cms/member.rb
@@ -1,3 +1,18 @@
 class Cms::Member
   include Cms::Member::Model
+
+  class << self
+    public
+      def create_with_omniauth(auth, site)
+        create!do |omniuser|
+          omniuser.site_id = site.id
+          omniuser.oauth_type = auth.provider
+          omniuser.oauth_id = auth.uid
+          omniuser.oauth_token = auth.credentials.token
+          omniuser.name = auth.info.name
+          omniuser.email = auth.info.email
+          omniuser.password = nil
+        end
+      end
+  end
 end
