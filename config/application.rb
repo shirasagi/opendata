4fe2b3a62aef60c3e4d85d19284d487ce45865b6
itowtips <ito@web-tips.co.jp>
2015-08-28 10:20:27 +0900

    v0.9.4

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module SS
-  mattr_reader(:version) { "0.9.3" }
+  mattr_reader(:version) { "0.9.4" }
 
   class Application < Rails::Application
     config.autoload_paths << "#{config.root}/lib"
