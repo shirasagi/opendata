7a919323730b49efb8856013057cff3248aed43a
itowtips <ito@web-tips.co.jp>
2015-11-02 19:49:53 +0900

    [add] workflow to od resources

--- app/models/cms/site.rb
+++ app/models/cms/site.rb
@@ -2,6 +2,7 @@ class Cms::Site
   include SS::Model::Site
   include Cms::SitePermission
   include Cms::Addon::PageSetting
+  include Opendata::Addon::MypageSetting
 
   set_permission_name "cms_sites"
 end
