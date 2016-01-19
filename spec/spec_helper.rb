d1e6facd4f407e1e879d0ad4d5e7fd4d3f36c534
ka <ka.kaosf@gmail.com>
2016-01-19 21:57:51 +0900

    [fix] Disable WebMock at first

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,6 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
 require 'database_cleaner'
-require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
---
+++ spec/support/webmock_helper.rb
@@ -0,0 +1,3 @@
+require 'webmock/rspec'
+include WebMock::API
+WebMock.disable!
