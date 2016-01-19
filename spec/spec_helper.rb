4fe2b434881646503f9212186a22c4f385e0210b
ka <ka.kaosf@gmail.com>
2016-01-19 21:52:49 +0900

    Revert "[fix] require webmock/rspec only in ckan model tests"
    This reverts commit fa597e806b28d1f173a7011f78f3df2dbbe28a4c.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
 require 'database_cleaner'
+require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
